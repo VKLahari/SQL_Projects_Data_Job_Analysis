7002814e485fecda8678a2d811ca87941e62da1c
VKLahari <vadalilahari@gmail.com>
2025-02-05 23:26:27 +0530

    Uploded months table database

---
+++ sql_load/Extracting_months.sql
@@ -0,0 +1,40 @@
+CREATE TABLE january_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=1;
+CREATE TABLE febraury_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=2;
+CREATE TABLE march_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=3;
+CREATE TABLE april_jobs AS 
+    SELECT * FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date)=4;
+CREATE TABLE jmay_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=5;
+CREATE TABLE june_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=6;
+CREATE TABLE july_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=7;
+CREATE TABLE august_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=8;
+CREATE TABLE september_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=9;
+CREATE TABLE october_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=10;
+CREATE TABLE november_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=11;
+CREATE TABLE december_jobs AS 
+    SELECT * FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=12;
+SELECT job_posted_date FROM january_jobs;
+SELECT s.skills,count(*) as most_mentioned FROM skills_dim s JOIN skills_job_dim sd ON s.skill_id=sd.skill_id group by s.skills ORDER BY most_mentioned DESC LIMIT 5;
+SELECT skills FROM skills_dim;
+SELECT skill_id FROM skills_job_dim;
